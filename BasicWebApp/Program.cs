283664ea5c7fa8919d58985d1aabc2a1419bec59
MarkBoyle1 <mark.boyle@myob.com>
2022-05-06 15:26:51 +1000

    Initial commit

---
+++ BasicWebApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicWebApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
